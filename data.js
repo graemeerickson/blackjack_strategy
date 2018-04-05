739524a87180304efb6482114f700a1f40444d5a
graemeerickson <graemeerickson@gmail.com>
2018-04-05 13:15:51 -0700

    Created 'deck' data structure (array of objects)

---
+++ data.js
@@ -0,0 +1,314 @@
+const deck = [
+   {
+      rank: 2,
+      suit: 'club',
+      value: 2,
+      played: false
+   },
+   {
+      rank: 2,
+      suit: 'diamond',
+      value: 2,
+      played: false
+   },
+   {
+      rank: 2,
+      suit: 'heart',
+      value: 2,
+      played: false
+   },
+   {
+      rank: 2,
+      suit: 'spade',
+      value: 2,
+      played: false
+   },
+   {
+      rank: 3,
+      suit: 'club',
+      value: 3,
+      played: false
+   },
+   {
+      rank: 3,
+      suit: 'diamond',
+      value: 3,
+      played: false
+   },
+   {
+      rank: 3,
+      suit: 'heart',
+      value: 3,
+      played: false
+   },
+   {
+      rank: 3,
+      suit: 'spade',
+      value: 3,
+      played: false
+   },
+   {
+      rank: 4,
+      suit: 'club',
+      value: 4,
+      played: false
+   },
+   {
+      rank: 4,
+      suit: 'diamond',
+      value: 4,
+      played: false
+   },
+   {
+      rank: 4,
+      suit: 'heart',
+      value: 4,
+      played: false
+   },
+   {
+      rank: 4,
+      suit: 'spade',
+      value: 4,
+      played: false
+   },
+   {
+      rank: 5,
+      suit: 'club',
+      value: 5,
+      played: false
+   },
+   {
+      rank: 5,
+      suit: 'diamond',
+      value: 5,
+      played: false
+   },
+   {
+      rank: 5,
+      suit: 'heart',
+      value: 5,
+      played: false
+   },
+   {
+      rank: 5,
+      suit: 'spade',
+      value: 5,
+      played: false
+   },
+   {
+      rank: 6,
+      suit: 'club',
+      value: 6,
+      played: false
+   },
+   {
+      rank: 6,
+      suit: 'diamond',
+      value: 6,
+      played: false
+   },
+   {
+      rank: 6,
+      suit: 'heart',
+      value: 6,
+      played: false
+   },
+   {
+      rank: 6,
+      suit: 'spade',
+      value: 6,
+      played: false
+   },
+   {
+      rank: 7,
+      suit: 'club',
+      value: 7,
+      played: false
+   },
+   {
+      rank: 7,
+      suit: 'diamond',
+      value: 7,
+      played: false
+   },
+   {
+      rank: 7,
+      suit: 'heart',
+      value: 7,
+      played: false
+   },
+   {
+      rank: 7,
+      suit: 'spade',
+      value: 7,
+      played: false
+   },
+   {
+      rank: 8,
+      suit: 'club',
+      value: 8,
+      played: false
+   },
+   {
+      rank: 8,
+      suit: 'diamond',
+      value: 8,
+      played: false
+   },
+   {
+      rank: 8,
+      suit: 'heart',
+      value: 8,
+      played: false
+   },
+   {
+      rank: 8,
+      suit: 'spade',
+      value: 8,
+      played: false
+   },
+   {
+      rank: 9,
+      suit: 'club',
+      value: 9,
+      played: false
+   },
+   {
+      rank: 9,
+      suit: 'diamond',
+      value: 9,
+      played: false
+   },
+   {
+      rank: 9,
+      suit: 'heart',
+      value: 9,
+      played: false
+   },
+   {
+      rank: 9,
+      suit: 'spade',
+      value: 9,
+      played: false
+   },
+   {
+      rank: 10,
+      suit: 'club',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 10,
+      suit: 'diamond',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 10,
+      suit: 'heart',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 10,
+      suit: 'spade',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 'jack',
+      suit: 'club',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 'jack',
+      suit: 'diamond',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 'jack',
+      suit: 'heart',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 'jack',
+      suit: 'spade',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 'queen',
+      suit: 'club',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 'queen',
+      suit: 'diamond',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 'queen',
+      suit: 'heart',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 'queen',
+      suit: 'spade',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 'king',
+      suit: 'club',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 'king',
+      suit: 'diamond',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 'king',
+      suit: 'heart',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 'king',
+      suit: 'spade',
+      value: 10,
+      played: false
+   },
+   {
+      rank: 'ace',
+      suit: 'club',
+      value: 1,
+      played: false
+   },
+   {
+      rank: 'ace',
+      suit: 'diamond',
+      value: 1,
+      played: false
+   },
+   {
+      rank: 'ace',
+      suit: 'heart',
+      value: 1,
+      played: false
+   },
+   {
+      rank: 'ace',
+      suit: 'spade',
+      value: 1,
+      played: false
+   }
+]
